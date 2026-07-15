bca482a22e8a9ccf5bf76484ac473380d884acb0
sabbirHossain <sabbirshnt@gmail.com>
2026-07-15 18:07:58 +0600

    done

--- src/pages/SportDetails/SportDetails.jsx
+++ src/pages/SportDetails/SportDetails.jsx
@@ -191,15 +191,16 @@ const SportDetails = () => {
                     ></iframe>
                   )}
                   {matchOdds?.length > 0 && <MatchOdds data={matchOdds} />}
-                  {data?.premium && data?.premium?.eventId && (
-                    <Premium premium={data?.premium} />
-                  )}
+
                   {bookmaker?.length > 0 && <Bookmaker data={bookmaker} />}
                   {data?.result?.length > 0 && <Fancy data={data?.result} />}
                   {eventTypeId == 7 || eventTypeId == 4339 ? (
                     <HorseGreyhoundSportsDetails data={data?.result} />
                   ) : null}
                   {tiedMatch?.length > 0 && <MatchOdds data={tiedMatch} />}
+                  {data?.premium && data?.premium?.eventId && (
+                    <Premium premium={data?.premium} />
+                  )}
                 </div>
 
                 <DesktopBetRightSidebar hasVideo={data?.score?.hasVideo} />
